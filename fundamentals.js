4d8e4df7af18272873b898627ab692bed39d9026
Abu Nayim Mohammad Rakib <118966477+webdiv-rakib@users.noreply.github.com>
2026-02-03 03:26:16 +0600

    Create fundamentals.js

---
+++ fundamentals.js
@@ -0,0 +1,8 @@
+/**
+ * 1. how to declare a variable using let and const
+ * 2. 6 basic condition >,<,===,!==,<=,>= and multiple conditions: && ,||
+ * 3. array[index,length,push,]
+ * 4. loop(for loop)
+ * 5. function(simple function,arrow function)
+ * 6. object
+ */
